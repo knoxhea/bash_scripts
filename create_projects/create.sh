2b85310f59e1016983520bff9d50fab3b51f1b80
knoxhea <heather.knox.92@gmail.com>
2020-07-05 15:35:39 -0500

    Added create script for projects and adding to git
    Adding file to create projects and add the initial commit to github.
    This project was created using this script

---
+++ create_projects/create.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#setup variables
+project_dir=$CODE_PROJECT_PATH
+project_name=$1
+username=$GITHUB_USERNAME
+request_data='{"name" : "'$project_name'"}'
+request_auth='-u '$username':'$GITHUB_TOKEN_AUTOMATED_PROJ
+
+#navigate to and create the project directory
+cd $project_dir
+mkdir $project_name
+cd $project_name
+
+#initialize git and create it in github
+git init
+curl "$request_auth" -X POST --data "$request_data" https://api.github.com/user/repos
+
+#specify the remotes, create a readme  and commit 
+git remote add origin git@github.com:$username/$project_name.git
+touch README.md
+git add .
+git commit -m "Initial commit"
+
+#push to git and open in vs code
+git push -u origin master
+code .
